b57c6b8efd62f6ee5a7cda9f9aaefdc4a9a1c479
William Ferguson <william.ferguson@xandar.com.au>
2013-02-14 19:50:09 +1000

    Splitting adapters and example into separate modules.

---
+++ example/src/main/java/au/com/xandar/admob/MyAdmobConfig.java
@@ -0,0 +1,10 @@
+package au.com.xandar.admob;
+
+/**
+ * Not to be committed as everyone needs to provide their own and this project has been made public.
+ */
+public class MyAdmobConfig {
+
+    // My CustomEvents-Test mediation Id
+    public static final String ADMOB_MEDIATION_ID = "7951d9d964134273";
+}
